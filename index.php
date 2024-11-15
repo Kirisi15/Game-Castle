1d0f0cdd7a12b5de1cb169020d861c5889627e6c
Kirisi15 <kkirisigan@gmail.com>
2024-11-15 23:17:32 +0530

    added style to index page

--- index.php
+++ index.php
@@ -4,8 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Game Castle</title>
+    <link rel="stylesheet" href="../Game-castle/Styling/index.css">
 </head>
 <body>
-    <h1>Sample</h1>
+    <header>
+        <h1>Game Castle</h1>
+    </header>
 </body>
 </html>
